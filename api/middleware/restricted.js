f83461a50dafead7013d6880b19197991b1482d2
Zachary Cooremans <zcooremans@gmail.com>
2021-07-23 13:00:20 -0500

    pushing

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,26 @@
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../auth/secrets')
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization
+  if (token) {
+    jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        next({
+          status: 401,
+          message: 'token invalid'
+        })
+      } else {
+        req.decodedJwt = decodedToken
+        next()
+      }
+    })
+  } else {
+    next({
+      status: 401,
+      message: 'token required'
+    })
+  }
   /*
     IMPLEMENT
 
